babb618ddc206ae5e0739b0d7f06343250a89caa
bishot <andres-120397@hotmail.com>
2020-01-08 21:44:02 -0500

    exercises

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,10 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js'
+
 const video = document.querySelector('video')
 const button = document.querySelector('button')
 
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()]});
 
-function MediaPlayer(config) {
-this.media = config.el;
-}
-
-MediaPlayer.prototype.togglePlay = function() {
-if (this.media.paused)
-    this.media.play();
-else 
-    this.media.pause();
-}
-const player = new MediaPlayer({ el: video });
 button.onclick = () => player.togglePlay();
 
